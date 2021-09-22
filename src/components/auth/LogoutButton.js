2ab20e8a648b22aecf600b702187ba53d9685c0a
Ross Correia <rmcorre@gmail.com>
2021-09-22 04:35:57 +0000

    Edit classes for AuthNav and LogoutButton to correct display and padding.

--- src/components/auth/LogoutButton.js
+++ src/components/auth/LogoutButton.js
@@ -4,16 +4,28 @@ import { useAuth0 } from '@auth0/auth0-react';
 const LogoutButton = () => {
   const { logout } = useAuth0();
   return (
-    <button
-      className="btn btn-link text-dark"
-      onClick={() =>
-        logout({
-          returnTo: window.location.origin,
-        })
-      }
-    >
-      Logout
-    </button>
+    <>
+      <button
+        className="btn btn-outline-secondary text-dark d-lg-inline-block d-none"
+        onClick={() =>
+          logout({
+            returnTo: window.location.origin,
+          })
+        }
+      >
+        Logout
+      </button>
+      <button
+        className="btn btn-sm btn-outline-secondary text-dark d-lg-none d-inline-block"
+        onClick={() =>
+          logout({
+            returnTo: window.location.origin,
+          })
+        }
+      >
+        Logout
+      </button>
+    </>
   );
 };
 
