616c3db7072c84a7d92fd45c7726bebee95b4f34
Ross Correia <rmcorre@gmail.com>
2021-09-22 19:26:41 +0000

    Add arrow-down icon to Download CV button. Edit alt text on heroImage.

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -24,7 +24,20 @@ const Hero = () => {
           </h2>
           <div className="wow fadeInUp text-nowrap" data-wow-delay=".7s">
             <a className="btn btn-translucent-primary me-3" href="/resume">
-              <i className="ai-arrow-down-circle mt-n1 me-2"></i>Download CV
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18"
+                height="18"
+                fill="currentColor"
+                className="bi bi-arrow-down-circle me-2"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"
+                />
+              </svg>
+              Download CV
             </a>
             <a
               className="btn btn-outline-primary"
@@ -36,7 +49,12 @@ const Hero = () => {
           </div>
         </div>
         <div className="col-lg-8 col-md-7 order-md-2 order-1 mb-md-0 mb-grid-gutter">
-          <img src={heroImage} alt="Illustration" rel="preload" as="image" />
+          <img
+            src={heroImage}
+            alt="Illustration of a developer"
+            rel="preload"
+            as="image"
+          />
         </div>
       </div>
     </section>
