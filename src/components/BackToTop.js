3e4c0599c150197843294e63226678836d30ddb4
Ross Correia <rmcorre@gmail.com>
2021-09-25 18:27:18 +0000

    Add listener for scroll position. Add handler to show button. Add up arrow icon for button.

--- src/components/BackToTop.js
+++ src/components/BackToTop.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
 const BackToTop = () => {
+  const [show, setShow] = useState('');
+
+  const handleShow = (e) => {
+    e.currentTarget.pageYOffset > 600 ? setShow('show') : setShow('');
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleShow);
+
+    return () => {
+      window.removeEventListener('scroll', handleShow);
+    };
+  }, []);
+
   return (
-    <a className="btn-scroll-top" href="#top" data-scroll data-fixed-element>
+    <a
+      className={`btn-scroll-top ${show}`}
+      href="#top"
+      data-scroll
+      data-fixed-element
+    >
       <span className="btn-scroll-top-tooltip text-muted fs-sm me-2">Top</span>
-      <i className="btn-scroll-top-icon ai-arrow-up"> </i>
+      <i className="btn-scroll-top-icon bi bi-arrow-up"> </i>
     </a>
   );
 };
